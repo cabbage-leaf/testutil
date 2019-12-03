ef4d7ce4032b7827eb877a9440e39d372dec6260
crocobaby <31754809+crocobaby@users.noreply.github.com>
2019-12-03 12:12:05 +0800

    更新
    更新

--- src/main/java/com/lh/test/Test6.java
+++ src/main/java/com/lh/test/Test6.java
@@ -17,6 +17,7 @@ public class Test6 {
 	
 //		ThreadUtil.newExecutor();
 	
+	// online test
 		
 		ConcurrentHashMap<String, String> map = new ConcurrentHashMap<String, String>();
 		System.out.println(map.put("1", "1"));
