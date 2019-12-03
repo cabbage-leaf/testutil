7188958b80a2668f4848cbdffcd37d09b26e53b2
crocobaby <31754809+crocobaby@users.noreply.github.com>
2019-12-03 14:34:43 +0800

    Update Test6.java
    bb

--- src/main/java/com/lh/test/Test6.java
+++ src/main/java/com/lh/test/Test6.java
@@ -18,7 +18,7 @@ public class Test6 {
 //		ThreadUtil.newExecutor();
 	
 	// online test aa
-	// online test  11
+	// online test  11 bb
 		
 		ConcurrentHashMap<String, String> map = new ConcurrentHashMap<String, String>();
 		System.out.println(map.put("1", "1"));
